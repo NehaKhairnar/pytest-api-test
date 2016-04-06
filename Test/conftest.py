12c7290953654950e9933f79f7835b63a31bd76b
milton <milton@sjinnovation.com>
2016-04-06 15:36:21 +0530

    made changes to conftest.py

--- Test/conftest.py
+++ Test/conftest.py
@@ -2,8 +2,8 @@
 import pytest
 
 
-aseurl = api.openweathermap.org/
-apikey = 6ceac7294286140698fbb9c2809b3a60
+baseurl = "http://api.openweathermap.org/data/2.5/"
+apikey = "6ceac7294286140698fbb9c2809b3a60"
 
 
 @pytest.fixture
