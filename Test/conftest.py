632cda462b5a0482bf120cae624ff533e3d7a5a7
milton <milton@sjinnovation.com>
2016-04-07 17:13:54 +0530

    Added new files

--- Test/conftest.py
+++ Test/conftest.py
@@ -4,7 +4,6 @@ import pytest
 
 
 
-
 @pytest.fixture
 def smtp():
     import smtplib
--- Test/test_404.py
+++ Test/test_404.py
@@ -1,7 +1,17 @@
+
+import base
 import pytest
 import requests
-import base
+@pytest.mark.parametrize("input,expected", [
+    ((base.baseurl + 'weather?q=London,uk&appid='+base.apikey), 'London')
+])
 
-r= requests.get(base.baseurl + 'weather?q=London,uk&appid='+base.apikey)
-data = r.json()
-print(data)
+def test_eval(input, expected):
+    r = requests.get(input)
+    data= r.text
+    assert r.text == expected
+    assert r.status_code==200
+    assert r.headers['Content-Type']=='text/plain'
+    return data
+
+print (data)
